de24c67bafab6c6e84b764ce35c190c56ea24025
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-09-10 16:50:41 +0530

    Dummy users added

--- firebase_setup/index.js
+++ firebase_setup/index.js
@@ -13,11 +13,24 @@ const createDummyUser = async () => {
       'Content-Type': 'application/json',
     }
   );
+
+  await axios.post(
+    `${process.env.BACKEND_URL}/api/v1/auth`,
+    {
+      emailAddress: 'already+test@used.com',
+      username: 'alreadyUsedtest',
+      password: 'testtest',
+      name: 'testtest',
+    },
+    {
+      'Content-Type': 'application/json',
+    }
+  );
 };
 
 const main = () => {
   createDummyUser()
-    .then(() => console.log('Dummy user created'))
+    .then(() => console.log('Dummy users created'))
     .finally(() => console.log('Script End'));
 };
 
